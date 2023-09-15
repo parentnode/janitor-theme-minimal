74f8a732c8f6d352ca2f36b6097277c3800433b2
kaestel <martin@think.dk>
2023-09-15 22:39:43 +0200

    Minimal install

---
+++ www/index.php
@@ -0,0 +1,25 @@
+<?php
+$access_item = false;
+if(isset($read_access) && $read_access) {
+	return;
+}
+
+include_once($_SERVER["FRAMEWORK_PATH"]."/config/init.php");
+
+
+$action = $page->actions();
+
+
+$page->bodyClass("front");
+$page->pageTitle("Frontpage");
+
+
+
+$page->page([
+	"templates" => "pages/front.php"
+]);
+exit();
+
+
+?>
+ 
